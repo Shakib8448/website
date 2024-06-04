8d70ba8668b2464ddc14709b5e577254a4323863
Shakib <ibrahimshakib44@gmail.com>
2024-06-04 19:35:08 +0600

    asdfsdf

--- Projects/todo-1/src/App.jsx
+++ Projects/todo-1/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
+import AddItem from "./components/AddItem"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vitee + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <center class="todo-content">
+      <AppName />
+      <AddTodo />
+      <AddItem />
+      <AddItem />
+      <AddItem />
+    </center>
+  );
 }
 
-export default App
+export default App;
---
+++ Projects/todo-1/src/components/AddItem.jsx
@@ -0,0 +1,21 @@
+function AddItem() {
+  return (
+    <div>
+        <div class="row">
+          <div class="col-6">
+            Miaw
+          </div>
+          <div class="col-4">
+            11/05/2024
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default AddItem;
